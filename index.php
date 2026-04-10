9b52a71457c2008ff3d7efeaa2884e1e4029409c
Hebert Bratefixe Alquimim <hebert@uni9.pro.br>
2026-04-10 09:21:20 -0300

    alterado

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
-echo "<h1>Render com PHP</h1>";
+if(count($_POST)>0){
+    echo "<h1>Render com PHP</h1>";
+    print_r($_POST);
+
+}
 ?>
+<form method="post" action="">
+
+   Nome <input type="text" name="nome">
+   <button>Enviar</button> 
+</form>
